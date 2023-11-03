db477eda0257ab99abb4931dcde891789ec54b6e
Thaiquocse <Thaiquocse@gmail.com>
2023-11-03 14:38:36 +0700

    Add project files.

---
+++ BusinessObjects/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public int Role { get; set; }
+    }
+}
---
+++ DataAccessObjects/DAOs/ProductDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects.DAOs
+{
+    internal class ProductDAO
+    {
+    }
+}
